2706bde2ef31e01e25ddb9005a1cc47cf5d3ecba
mash197 <mohamedashraf8997@gmail.com>
2017-07-11 13:45:29 +0200

    adding a var

--- BackendSample/Controllers/HomeController.cs
+++ BackendSample/Controllers/HomeController.cs
@@ -24,7 +24,7 @@ namespace BackendSample.Controllers
             HttpContext.Response.AppendHeader("Access-Control-Allow-Headers", "access-control-allow-origin,content-type");
             HttpContext.Response.AppendHeader("Access-Control-Allow-Methods", "GET");
             int y = 0;
-
+            int a = 0;
             if (t % 2 == 0)
             {
                 x[n] = "O";
